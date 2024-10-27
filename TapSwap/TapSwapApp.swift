f803e599bad6ea624d851dd5ca28d027a2f19005
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-10-27 09:23:58 +0100

    Initial Commit

---
+++ TapSwap/TapSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapSwapApp.swift
+//  TapSwap
+//
+//  Created by MacBook AIR on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TapSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
